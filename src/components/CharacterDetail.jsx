d1fadfe739d66463d4181c8feee89fdc3d138c98
Harlxck <brunogustavo68@gmail.com>
2025-04-23 12:32:11 +0200

    Details section finished and some other minor changes

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./CharacterDetail.css";
 import { useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
 import UnlikedHeart from "../icons/UnlikedHeart";
+import CardHeart from "../icons/CardHeart";
 
-function CharacterDetail() {
+function CharacterDetail({ likedCharacters, setLikedCharacters }) {
   const { id } = useParams();
   const [character, setCharacter] = useState(null);
 
@@ -21,23 +21,37 @@ function CharacterDetail() {
 
   if (!character) return null;
 
+  const isLiked = likedCharacters.includes(character.id);
+
+  const handleLikeClick = () => {
+    if (isLiked) {
+      setLikedCharacters(likedCharacters.filter((cid) => cid !== character.id));
+    } else {
+      setLikedCharacters([...likedCharacters, character.id]);
+    }
+  };
+
   const comicsSorted = [...character.comics].sort((a, b) => {
     const dateA = new Date(a.date);
     const dateB = new Date(b.date);
-    return dateB - dateA; // Orden descendente (más recientes primero)
+    return dateB - dateA;
   });
 
   return (
     <>
-      {" "}
-     
       <div className="character-detail-header-background">
         <img src={character.path} alt={character.name} />
         <div className="character-detail">
           <div className="character-detail-header">
             <div className="character-detail-header-title">
               <h2>{character.name}</h2>
-              <UnlikedHeart width="24" height="22" />
+              <div onClick={handleLikeClick} style={{ cursor: "pointer" }}>
+                {isLiked ? (
+                  <CardHeart width="24" height="22" fill="#EC1D24" />
+                ) : (
+                  <UnlikedHeart width="24" height="22" />
+                )}
+              </div>
             </div>
             <div className="character-description">
               <p>{character.description}</p>
@@ -54,14 +68,17 @@ function CharacterDetail() {
             {comicsSorted.map((comic) => (
               <div key={comic.id} className="comic-item">
                 <img src={comic.path} alt={comic.title} />
-                <h3>{comic.title}</h3>
-                <h5>{comic.date ? comic.date.slice(0, 4) : ""}</h5>
+                <div className="comic-info-container">
+                  <h3 className="comic-title">{comic.title}</h3>
+                  <h5 className="comic-date">
+                    {comic.date ? comic.date.slice(0, 4) : ""}
+                  </h5>
+                </div>
               </div>
             ))}
           </div>
         </div>
       </div>
-      
     </>
   );
 }
